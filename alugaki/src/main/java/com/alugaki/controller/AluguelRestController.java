a6818ef5644417cbc3ffe61eaf7174e53fe3be89
josemalyson <josemalyson@gmail.com>
2016-09-09 14:26:25 -0300

    Commit correcao da forma de retorno para o angularjs

--- alugaki/src/main/java/com/alugaki/controller/AluguelRestController.java
+++ alugaki/src/main/java/com/alugaki/controller/AluguelRestController.java
@@ -5,7 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.alugaki.model.Aluguel;
@@ -17,7 +18,8 @@ public class AluguelRestController {
 	@Autowired
 	private AluguelService aluguelService;
 	
-	@RequestMapping("/aluguel")
+	@GetMapping("/aluguel")
+	@CrossOrigin(origins = "http://localhost:4200")
 	public ResponseEntity<List<Aluguel>> findAll(){
 		return new ResponseEntity<>(aluguelService.findaAll(), HttpStatus.OK);
 	}
--- alugaki/src/main/java/com/alugaki/controller/VeiculoRestController.java
+++ alugaki/src/main/java/com/alugaki/controller/VeiculoRestController.java
@@ -5,7 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.alugaki.model.Veiculo;
@@ -17,7 +18,8 @@ public class VeiculoRestController {
 	@Autowired
 	private VeiculoService veiculoService;
 
-	@RequestMapping("/veiculo")
+	@GetMapping("/veiculo")
+	@CrossOrigin(origins = "http://localhost:4200")
 	public ResponseEntity<List<Veiculo>> findAll() {
 		return new ResponseEntity<>(veiculoService.findaAll(), HttpStatus.OK);
 	}
